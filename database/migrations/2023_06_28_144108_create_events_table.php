9219a6fa7e10e9afb0fb4e615eb1f291d4e4cbf2
ahzare <a.zare137792@yahoo.com>
2023-06-28 18:53:58 +0330

    creating Event model and migration

---
+++ database/migrations/2023_06_28_144108_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('desc');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->unsignedBigInteger('by_admin_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('by_admin_id')->references('id')->on('admins')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
