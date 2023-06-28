4839878bff720d73629b4bbbbd30a96a8c8ec46f
ahzare <a.zare137792@yahoo.com>
2023-06-28 19:39:03 +0330

    adding wallet after column

--- database/migrations/2023_06_28_152203_add_wallet_to_users_table.php
+++ database/migrations/2023_06_28_152203_add_wallet_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('wallet')->default(0);
+            $table->unsignedBigInteger('wallet')->default(0)->after('password');
         });
     }
 
