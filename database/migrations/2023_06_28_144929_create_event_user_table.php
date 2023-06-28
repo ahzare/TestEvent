923e129844f94a8b9d2b259c4a8ba6092fe590ac
ahzare <a.zare137792@yahoo.com>
2023-06-28 18:56:02 +0330

    creating event_user migration

---
+++ database/migrations/2023_06_28_144929_create_event_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('event_id');
+            $table->dateTime('registered_date');
+            $table->unsignedBigInteger('earned')->default(0);
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('admins')
+                ->onDelete('cascade');
+            $table->foreign('event_id')->references('id')->on('events')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_event');
+    }
+};
